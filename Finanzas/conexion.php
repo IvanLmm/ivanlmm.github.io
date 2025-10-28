8a304cfca54b81993698ad58b3ca96229384ad0b
IvanLmm <ivanlmmaradiaga@gmail.com>
2025-10-27 22:09:03 -0600

    Update conexion.php

--- Finanzas/conexion.php
+++ Finanzas/conexion.php
@@ -1,18 +1,15 @@
 <?php
-$server = "4.206.135.183"; 
-$database = "Proyecto";
-$user = "orlynk";
-$password = "12345678";
+$server = "4.206.135.183";
+$connectionOptions = [
+  "Database" => "Proyecto",
+  "Uid" => "orlynk",
+  "PWD" => "12345678",
+  "Encrypt" => true,                 // cifrado ON
+  "TrustServerCertificate" => true   // <- acepta certificado autofirmado
+];
 
-$conn = sqlsrv_connect($server, [
-    "Database" => $database,
-    "UID" => $user,
-    "PWD" => $password,
-    "Encrypt" => true,
-    "TrustServerCertificate" => false
-]);
-
-if (!$conn) {
+$conn = sqlsrv_connect($server, $connectionOptions);
+if ($conn === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 echo "Conexión exitosa a SQL Server.";
